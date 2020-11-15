a3e667e22a98d92463c4b54ffa80dc313a762a4d
WangAo <2455531510@qq.com>
2020-11-15 22:35:14 +0800

    the fourth update

--- MyProject/app.py
+++ MyProject/app.py
@@ -1,25 +1,35 @@
 from flask import Flask, render_template, request, redirect
+from users import Users
 
 app = Flask(__name__)
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
         return render_template('register.html')
-    return render_template('register.html')
+    return '注册成功'
 
 
 @app.route('/', methods=['GET', 'POST'])
-def login():
+def index():
     return render_template('index.html')
+
+
+@app.route('/login',methods=['GET', 'POST'])
+def login():
+    pass
 
 
 @app.route('/search', methods=['GEI', 'POST'])
 def search():
-    if request.method == 'POST':
-        return render_template('/search.html')
-    return render_template('search1.html')
+    user1 = Users(1, '小明', '123456')
+    username = request.form.get('username')
+    print(username)
+    password = request.form.get('password')
+    if username == user1.username and password == user1.password:
+        return render_template('search.html', user=user1)
+    return render_template('index.html', msg='账号或密码不正确', username=username)
 
 
 if __name__ == '__main__':
