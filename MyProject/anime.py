24d03c1a000451b863319bb3a66a1cfba9436ba2
WangAo <2455531510@qq.com>
2020-11-15 16:51:19 +0800

    the third update

--- MyProject/anime.py
+++ MyProject/anime.py
@@ -1,2 +1,8 @@
-class Arime():
-    
+class Arimes():
+    def __init__(self, id, name, brief):
+        self.id = id
+        self.name = name
+        self.brief = brief
+
+    def __str__(self):
+        print(self.name)
--- MyProject/app.py
+++ MyProject/app.py
@@ -1,15 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
+@app.route('/register')
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    return render_template('register.html')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
     return render_template('index.html')
 
 
 @app.route('/search', methods=['GEI', 'POST'])
 def search():
+    if request.method == 'POST':
+        return render_template('/search.html')
     return render_template('search1.html')
 
 
