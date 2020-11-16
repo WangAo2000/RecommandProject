22e451c94691f7efe5316247bdab587b5da26ce6
WangAo <2455531510@qq.com>
2020-11-16 13:26:39 +0800

    the fifth update

--- MyProject/app.py
+++ MyProject/app.py
@@ -1,14 +1,24 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for, flash
 from users import Users
 
 app = Flask(__name__)
+app.secret_key = 'this is a secret key'
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        username = request.form.get('usename')
+        password = request.form.get('password')
+        repassword = request.form.get('repassword')
+        msg = '密码不正确'
+        if password == repassword:
+            return redirect('/register_succeed')
+        else:
+            return render_template('register.html', msg=msg)
     if request.method == 'GET':
         return render_template('register.html')
-    return '注册成功'
+    return render_template('register.html')
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -16,20 +26,21 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/login',methods=['GET', 'POST'])
-def login():
-    pass
+@app.route('/register_succeed', methods=['GET', 'POST'])
+def register_succeed():
+    username = request.form.get('usename')
+    password = request.form.get('password')
+    return render_template('register_succeed.html')
 
 
 @app.route('/search', methods=['GEI', 'POST'])
 def search():
     user1 = Users(1, '小明', '123456')
     username = request.form.get('username')
-    print(username)
     password = request.form.get('password')
     if username == user1.username and password == user1.password:
         return render_template('search.html', user=user1)
-    return render_template('index.html', msg='账号或密码不正确', username=username)
+    return render_template('index.html', msg='账号或密码不正确')
 
 
 if __name__ == '__main__':
